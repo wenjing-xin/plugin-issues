f4da4ae50e66777c637d95d4ecb0bd1386535b85
wenjing-xin <wb3368316006@gmail.com>
2025-08-16 15:36:32 +0800

    移除无用文件

--- src/main/java/com/webjing/issues/IssuesPlugin.java
+++ src/main/java/com/webjing/issues/IssuesPlugin.java
@@ -5,9 +5,7 @@ import com.webjing.issues.extension.Issue;
 import com.webjing.issues.extension.IssueLabel;
 import com.webjing.issues.extension.IssueSubject;
 import com.webjing.issues.extension.IssueTemplate;
-import com.webjing.mandate.auth.WebjingPluginAuthManager;
 import org.apache.commons.lang3.BooleanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import run.halo.app.extension.SchemeManager;
 import run.halo.app.extension.index.IndexSpec;
@@ -27,9 +25,6 @@ import static run.halo.app.extension.index.IndexAttributeFactory.simpleAttribute
 @Component
 public class IssuesPlugin extends BasePlugin {
 
-    // @Autowired
-    // private WebjingPluginAuthManager webjingPluginAuthManager;
-
     private final SchemeManager schemeManager;
 
     public IssuesPlugin(PluginContext pluginContext, SchemeManager schemeManager) {
@@ -39,8 +34,6 @@ public class IssuesPlugin extends BasePlugin {
 
     @Override
     public void start() {
-
-        // webjingPluginAuthManager.pluginStartCheck();
 
         schemeManager.register(IssueSubject.class, indexSpecs -> {
             indexSpecs.add(new IndexSpec()
